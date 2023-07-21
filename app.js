ac02914682a771d9a8f20b61dfbf34ec58898a10
Damla Oksel <damlaoksel@hotmail.com>
2023-07-22 01:27:24 +0300

    her bir todoya id verildi

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-let todoForm=document.querySelector("#todoForm");
-let results=document.querySelector("#results");
+let todoSub = document.querySelector("#todoSub");
+let deleteAll = document.querySelector("#todoDeleteAll");
+let results = document.querySelector("#results");
+let counter = 0;
 
 EventListeners();
-function EventListeners(){
-    todoForm.addEventListener("submit", ToDoAdd)
+function EventListeners() {
+    todoSub.addEventListener("click", ToDoAdd)
 }
-function ToDoAdd(e){
+function ToDoAdd(e) {
     e.preventDefault();
     GetValue();
 }
-function GetValue(){
-    let todoInput=document.querySelector("#todoInput").value.trim();
-    let resultLi=document.createElement("li");
+function GetValue() {
+    let todoInput = document.querySelector("#todoInput").value.trim();
+    let resultLi = document.createElement("li");
+    let isFull = "empty";
     resultLi.classList.add("result");
-    resultLi.innerHTML=`
-    <button type="button" id="done"><img id="empty" src="Images/empty.png"></button>
-    <p>${todoInput}</p>
-    <button type="button" id="btn-remove">X</button>`;
+    counter++;
+    resultLi.id = counter;
+    localStorage.setItem(`todoInput${counter}`, JSON.stringify(todoInput));
+    console.log(resultLi);
+    resultLi.innerHTML = `
+      <button type="button" id="done"><img src="Images/${isFull}.png"></button>
+      <p>${JSON.parse(localStorage.getItem(`todoInput${counter}`))}</p>
+      <button type="button" id="btn-remove">X</button>
+    `;
     results.append(resultLi);
+    //localStorage.clear();
 }
