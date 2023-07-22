28c042063e60c4226e2dd205a6c3f11770595258
Damla Oksel <damlaoksel@hotmail.com>
2023-07-22 13:03:37 +0300

    localstorage ile veriler saklandı

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let results = document.querySelector("#results");
 let counter = 0;
 
 EventListeners();
+LoadTodosFromLocalStorage(); // Sayfa yüklendiğinde localStorage'dan görevleri yükle.
 function EventListeners() {
     todoSub.addEventListener("click", ToDoAdd)
 }
@@ -27,4 +28,21 @@ function GetValue() {
     `;
     results.append(resultLi);
     //localStorage.clear();
+}
+function LoadTodosFromLocalStorage() {
+    for (let i = 1; i <= localStorage.length; i++) {
+      let todoInput = JSON.parse(localStorage.getItem(`todoInput${i}`));
+      if (todoInput !== null) {
+        let resultLi = document.createElement("li");
+        let isFull = "empty";
+        resultLi.classList.add("result");
+        resultLi.id = i;
+        resultLi.innerHTML = `
+          <button type="button" id="done"><img src="Images/${isFull}.png"></button>
+          <p>${todoInput}</p>
+          <button type="button" id="btn-remove">X</button>
+        `;
+        results.append(resultLi);
+      }
+    }
 }
